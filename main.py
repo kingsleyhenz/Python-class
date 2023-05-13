947dff4619ba84d8bf3e074715691799d193407e
BLACKHENZ29 <ayehenz29@gmail.com>
2023-05-13 07:19:03 +0100

    loops in python

---
+++ main.py
@@ -0,0 +1,42 @@
+# for a in range(5):
+#     for b in range(3):
+#         print(f"({a},{b})")
+
+
+# for letter in "Kingsley":
+#     print(f"{letter} is a letter")
+
+
+# for no in [1,2,3,4,5,6]:
+#     print(f"{no}")
+
+# WHILE LOOP
+# amount = 200
+# while amount > 0:
+#     print(f"{amount}")
+#     amount //= 2
+
+# option = "no"
+ 
+# while option.lower() != "no":
+#     option = input(">")
+#     print("PRINT",option) 
+    
+# print("Bye Bye  ")    
+
+
+# Infinite loop   
+# while True:
+#     option = input(">")
+#     print("PRINT",option)
+#     if option.lower() == "no":
+#         break   
+
+
+# count = 0
+# for number in range(1,10):
+#     if number % 2 == 0:
+#         count += 1
+# print(f"the number of even is :{count}")
+
+
