b7e469e8f271032570d00cbbf172575d360afc75
BLACKHENZ29 <ayehenz29@gmail.com>
2023-05-13 07:23:04 +0100

    CRUD operations for lists

--- main.py
+++ main.py
@@ -75,3 +75,63 @@
 # lname = input(" => Enter your lastname => ")
 # print(user_signup(fname,lname,"active"))
 
+# list
+
+letters = ["a", "b", "c", "d", "e", "f"]
+# print(letters[0])
+# zero = [0] * 50
+# print(zero)
+# add = zero + letters
+
+num = list(range(31))
+# print(num)
+
+characters = list("King Henz")
+# print(len(characters))  
+
+#last
+# print(letters[-1])
+
+# last two
+# print(letters[1:3])
+
+
+mylen = len(letters)
+# print(mylen)
+# print(letters[:mylen])
+
+#reverse 
+# print(letters[::-1])
+
+#list unpacking
+prices = [20,30,50,90,1000,45,2345]
+first,second,*third = prices
+# print(f"first={first}, second={second}, third={third}")
+
+# for index,chara in enumerate(letters):
+    # print(index, chara)
+    
+# letters.append("f")
+# print(letters)
+# letters.insert(2,"Naomi")
+# print(letters)
+
+#remove
+# letters.pop(2)
+# print(letters)
+
+#remove if index is now
+# letters.remove("b")
+# print(letters)
+
+#delete
+# del letters[1:3]
+# print(letters)
+
+#clear
+# letters.clear()
+# print(letters)
+
+# print(letters.index("a"))
+
+
