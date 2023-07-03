7e5e1ebc2f90c277482030e4b0a520cc4a47e86a
BLACKHENZ29 <ayehenz29@gmail.com>
2023-07-03 20:38:44 +0100

    Added Values and retrieved data type of skills

--- Tasks/Dictionary/task.py
+++ Tasks/Dictionary/task.py
@@ -6,16 +6,20 @@ dog = {'name': 'Fido', 'color': 'brown', 'breed': 'Labrador Retriever', 'legs': 
 # print(dog)
 # 3. Create a student dictionary and add first_name, last_name, gender, age, marital status, skills, country, city and address as keys for the dictionary
 student = {
-    'first_name': '',
-    'last_name': '',
-    'gender': '',
-    'age': '',
-    'marital_status': '',
-    'skills': '',
-    'country': '',
-    'city': '',
-    'address': ''
+    'first_name': 'King',
+    'last_name': 'Henz',
+    'gender': 'Male',
+    'age': '34',
+    'marital_status': 'Happily Married',
+    'skills': ['Football', 'Tennis', 'BasketBall', 'NodeJs'],
+    'country': 'Nigeria',
+    'city': 'Port-Harcourt',
+    'address': 'Woji'
 }
 # 4.Get the length of the student dictionary 
 length = len(student)
-print(length)
+# print(length)
+# 5. Get the value of skills and check the data type, it should be a list
+skill_count = student['skills']
+print(skill_count)
+print(type(skill_count))
