7b493a7a6fc150547333c53d56b94eff370a243b
BLACKHENZ29 <ayehenz29@gmail.com>
2023-07-03 20:55:46 +0100

    Initialized tuple and joined two diff tuples

---
+++ Tasks/Tuple/tuple.py
@@ -0,0 +1,15 @@
+# TUPLE
+# 1. Create an empty
+my_tuple = ()
+
+# 2. Create a tuple containing names of your sisters and your brothers
+sisters = ('Sophia', 'Nana', 'Sophia')
+brothers = ('Jedi', 'Henz', 'Swagga')
+# print(sisters)
+# print(brothers)
+# 3. Join brothers and sisters tuples and assign it to siblings
+siblings = brothers + sisters
+# print(siblings)
+# 4. How many siblings do you have?
+# 5. Modify the siblings tuple and add the name of your father and mother and assign it to family_members
+
