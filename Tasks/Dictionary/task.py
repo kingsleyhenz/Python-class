42af8b386a37c997d1fd2b0c5c1cfef3519101e5
BLACKHENZ29 <ayehenz29@gmail.com>
2023-07-03 20:21:58 +0100

    initialization of dictionary with data

---
+++ Tasks/Dictionary/task.py
@@ -0,0 +1,8 @@
+# DICTIONARY
+# 1. CREATE AN EMPTY DICTIONARY CALLED DOG
+dog = {}
+# 2. Add name, color, breed, legs, age to the dog dictionary
+dog = {'name': 'Fido', 'color': 'brown', 'breed': 'Labrador Retriever', 'legs': 4, 'age': 5}
+# print(dog)
+
+
