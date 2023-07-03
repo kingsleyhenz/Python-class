e7d0fcb1c010c1084dc48a0e0469d8676fd1c2be
BLACKHENZ29 <ayehenz29@gmail.com>
2023-07-03 20:45:57 +0100

    Got Dictionary Keys As Lists

--- Tasks/Dictionary/task.py
+++ Tasks/Dictionary/task.py
@@ -1,9 +1,11 @@
 # DICTIONARY
 # 1. CREATE AN EMPTY DICTIONARY CALLED DOG
 dog = {}
+
 # 2. Add name, color, breed, legs, age to the dog dictionary
 dog = {'name': 'Fido', 'color': 'brown', 'breed': 'Labrador Retriever', 'legs': 4, 'age': 5}
 # print(dog)
+
 # 3. Create a student dictionary and add first_name, last_name, gender, age, marital status, skills, country, city and address as keys for the dictionary
 student = {
     'first_name': 'King',
@@ -16,14 +18,21 @@ student = {
     'city': 'Port-Harcourt',
     'address': 'Woji'
 }
+
 # 4.Get the length of the student dictionary 
 length = len(student)
 # print(length)
+
 # 5. Get the value of skills and check the data type, it should be a list
 skill_count = student['skills']
 # print(skill_count)
 # print(type(skill_count))
+
 # 6. Modify the skills values by adding one or two skills
 student['skills'].append('MongoDb')
 student['skills'].append('ReactJs')
 # print(student['skills'])
+
+# 7. Get the dictionary keys as a list
+keys_list = list(student.keys())
+print(keys_list)
