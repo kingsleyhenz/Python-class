aa17aceb570759ad9a32574b23f9a576d3d1eb9f
BLACKHENZ29 <ayehenz29@gmail.com>
2023-07-03 21:03:02 +0100

    Created and joined three tuples

--- Tasks/Tuple/tuple.py
+++ Tasks/Tuple/tuple.py
@@ -17,3 +17,24 @@ num_siblings = len(siblings)
 family_members = siblings + ('Ayo', 'Kate')
 # print(family_members)
 
+
+
+# LEVEL 2
+# 1. Unpack siblings and parents from family_members
+siblings = family_members[:-2]
+parents = family_members[-2:]
+# 2. Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+fruits = ('apple', 'banana', 'orange')
+vegetables = ('carrot', 'spinach', 'tomato')
+animal_products = ('meat', 'milk', 'eggs')
+food_stuff_tp = fruits + vegetables + animal_products
+# print(food_stuff_tp)
+# 3. Change the about food_stuff_tp tuple to a food_stuff_lt list
+# 4. Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+# 5. Slice out the first three items and the last three items from food_staff_lt list
+# 6. Delete the food_staff_tp tuple completely
+# 7. Check if an item exists in tuple:
+# 8. Check if 'Estonia' is a nordic country
+# 9. Check if 'Iceland' is a nordic country
+# nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+
