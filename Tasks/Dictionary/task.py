8c9c9aeafec770d148ea15f690df85ddc64b6211
BLACKHENZ29 <ayehenz29@gmail.com>
2023-07-03 20:25:30 +0100

    dictionary(student) with fields

--- Tasks/Dictionary/task.py
+++ Tasks/Dictionary/task.py
@@ -4,5 +4,17 @@ dog = {}
 # 2. Add name, color, breed, legs, age to the dog dictionary
 dog = {'name': 'Fido', 'color': 'brown', 'breed': 'Labrador Retriever', 'legs': 4, 'age': 5}
 # print(dog)
+# 3. Create a student dictionary and add first_name, last_name, gender, age, marital status, skills, country, city and address as keys for the dictionary
+student = {
+    'first_name': '',
+    'last_name': '',
+    'gender': '',
+    'age': '',
+    'marital_status': '',
+    'skills': '',
+    'country': '',
+    'city': '',
+    'address': ''
+}
 
 
